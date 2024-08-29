293e04eafaed414c3baca950d8bfb591f257a975
Darwin Quiñones <darwinquinones735@gmail.com>
2024-08-29 11:38:26 -0500

    React Server and Client Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+
+
 import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  console.log('Hello world from home page')
+
   return (
     <main className={styles.main}>
       <h1>Hello world!! </h1>
